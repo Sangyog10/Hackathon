037b21f9a5354702343fb9e5e99a677f3bc7c116
sangyog <sangyog.puri111@gmail.com>
2024-01-19 21:23:39 +0545

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,47 +12,24 @@ const register=async(req,res)=>{
 }
 
 
-const login=async(req,res)=>{}
+const login=async(req,res)=>{
+    const { number, password } = req.body;
+    if (!number || !password) {
+      throw new CustomError.BadRequestError("Enter both number and password");
+    }
+    const user = await User.findOne({ number });
+    if (!user) {
+      throw new CustomError.UnauthenticatedError("No user with given Number found");
+    }
+  
+    const isPassword = await user.comparePassword(password);
+    if (!isPassword) {
+      throw new CustomError.UnauthenticatedError("Invalid password");
+    }
+    res.status(StatusCodes.CREATED).json({ user });
+}
 
 
 
 
 module.exports={register,login}
-
-
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-// const { StatusCodes } = require('http-status-codes');
-// const CustomError = require('../errors');
-// const User = require('../model/userModel');
-
-// const register = async (req, res) => {
-//   const { name, age, number, address, gender, password } = req.body;
-//   if (!password) {
-//     throw new CustomError.BadRequestError('Please provide all credentials');
-//   }
-
-//   try {
-//     // const hashedPassword = await bcrypt.hash(password, 10);
-
-//     const user = await User.create({
-//       name,
-//       age,
-//       number,
-//       address,
-//       gender,
-//       password: hashedPassword,
-//     });
-
-//     res.status(StatusCodes.OK).json({ msg: 'User registered successfully' });
-//   } catch (error) {
-//     console.error(error);
-//     throw new CustomError.InternalServerError('Error registering user');
-//   }
-// };
-
-// const login = async (req, res) => {
-//   // Implementation for login goes here
-// };
-
-// module.exports = { register, login };
