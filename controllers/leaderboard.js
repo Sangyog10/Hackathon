3d7ed31e2d353eb83af8b0ad92cd01cbab547dce
sangyog <sangyog.puri111@gmail.com>
2024-01-20 14:07:15 +0545

    her

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -6,7 +6,7 @@ const CustomError = require("../errors");
 
 const getLeaderBoard=async(req,res)=>{
     const user = await User.find({}).sort({ points: -1 }).select('name address points');
-    res.status(StatusCodes.OK).json(user)
+    res.status(StatusCodes.OK).json({leaderboard:user})
 
 }
 
